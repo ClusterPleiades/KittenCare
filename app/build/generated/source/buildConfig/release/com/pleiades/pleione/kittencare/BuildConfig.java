e6f6877e90f2b7c155201b2fadb2458ae3714313
Cluster Pleiades <Pleione.P.Cluster@gmail.com>
2022-08-25 11:03:54 +0900

    Update to 2.1.1

--- app/build/generated/source/buildConfig/release/com/pleiades/pleione/kittencare/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/pleiades/pleione/kittencare/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.pleiades.pleione.kittencare";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 35;
-  public static final String VERSION_NAME = "2.1.0";
+  public static final int VERSION_CODE = 36;
+  public static final String VERSION_NAME = "2.1.1";
 }
